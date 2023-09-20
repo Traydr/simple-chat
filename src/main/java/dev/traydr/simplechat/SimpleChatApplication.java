f0a0cb05d532a2ab6d4bc75578493329a914ce99
Traydr <Traydr@protonmail.com>
2023-09-20 14:43:28 +0300

    feat: add init for spring quickstart

--- src/main/java/dev/traydr/simplechat/SimpleChatApplication.java
+++ src/main/java/dev/traydr/simplechat/SimpleChatApplication.java
@@ -2,12 +2,21 @@ package dev.traydr.simplechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SimpleChatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleChatApplication.class, args);
 	}
 
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
