ff0ecba053c226b5d9236bf7d5cfdfed50bab402
Traydr <Traydr@protonmail.com>
2023-09-22 10:02:37 +0300

    feat: create User object

---
+++ src/main/java/dev/traydr/simplechat/models/User.java
@@ -0,0 +1,13 @@
+package dev.traydr.simplechat.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long uid;
+}
