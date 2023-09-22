73a6596733bb6fcb9c6b632e1454c455db1e8ca1
Traydr <Traydr@protonmail.com>
2023-09-22 14:35:29 +0300

    feat: add request map to status resource

--- src/main/java/dev/traydr/simplechat/resources/StatusResource.java
+++ src/main/java/dev/traydr/simplechat/resources/StatusResource.java
@@ -1,17 +1,19 @@
 package dev.traydr.simplechat.resources;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/status")
 public class StatusResource {
-    @GetMapping("/status/server")
+    @GetMapping("/server")
     public String serverStatus() {
         return "Still here!";
     }
 
-    @GetMapping("/status/db")
+    @GetMapping("/db")
     public String databaseStatus() {
         return "Not implemeted";
     }
