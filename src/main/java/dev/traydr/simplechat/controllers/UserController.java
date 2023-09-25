49c8ab916862201959eafe1e5951046d6601d5aa
Traydr <Traydr@protonmail.com>
2023-09-25 20:14:03 +0300

    feat: attempt to get user JPA working

--- src/main/java/dev/traydr/simplechat/controllers/UserController.java
+++ src/main/java/dev/traydr/simplechat/controllers/UserController.java
@@ -1,7 +1,41 @@
 package dev.traydr.simplechat.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import dev.traydr.simplechat.entities.User;
+import dev.traydr.simplechat.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
+
+@RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public Optional<User> findById(@PathVariable int id) {
+        return userService.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    /* make this only update password and username
+    @PutMapping
+    public User update(@RequestBody User user) {
+        return userService.save(user);
+    }
+     */
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable int id) {
+        userService.deleteById(id);
+    }
 }
--- src/main/java/dev/traydr/simplechat/repositories/UserRepository.java
+++ src/main/java/dev/traydr/simplechat/repositories/UserRepository.java
@@ -4,5 +4,5 @@ import dev.traydr.simplechat.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    User findById(int id);
 }
