f974f8e735929c94f6021fa165c38ab40ff309fa
Traydr <Traydr@protonmail.com>
2023-09-25 18:42:31 +0300

    feat: create user repository

---
+++ src/main/java/dev/traydr/simplechat/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package dev.traydr.simplechat.repositories;
+
+import dev.traydr.simplechat.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
