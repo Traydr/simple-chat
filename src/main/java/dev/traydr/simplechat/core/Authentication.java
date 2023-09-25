3faaebc4667ea5b4a012fdf956cebba033473ce8
Traydr <Traydr@protonmail.com>
2023-09-26 01:06:02 +0300

    feat: init some functions for authentication

--- src/main/java/dev/traydr/simplechat/core/Authentication.java
+++ src/main/java/dev/traydr/simplechat/core/Authentication.java
@@ -1,4 +1,26 @@
 package dev.traydr.simplechat.core;
 
+import dev.traydr.simplechat.entities.Group;
+import dev.traydr.simplechat.entities.User;
+import dev.traydr.simplechat.repositories.GroupRepository;
+import dev.traydr.simplechat.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class Authentication {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+     public boolean login(String username, String password) {
+         // Todo: return token on valid credentials
+         return false;
+    }
+
+    public boolean validToken(String token, long id) {
+         return false;
+    }
 }
