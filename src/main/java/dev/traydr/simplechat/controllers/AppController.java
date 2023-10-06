6c27f3aa8d83f99efdf5eb67ab4b108c72d82c76
Traydr <Traydr@protonmail.com>
2023-10-06 19:59:25 +0300

    feat: final commit before changing to c#

--- src/main/java/dev/traydr/simplechat/controllers/AppController.java
+++ src/main/java/dev/traydr/simplechat/controllers/AppController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
+    @GetMapping("")
+    public String index() {
+        return "index";
+    }
+
     @GetMapping("/register")
     public String register() {
         return "register";
