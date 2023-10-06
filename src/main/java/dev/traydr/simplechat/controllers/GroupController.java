ac18bca4860b53894257d5a053b0450f24533f36
Traydr <Traydr@protonmail.com>
2023-10-06 16:30:29 +0300

    feat: get groups

--- src/main/java/dev/traydr/simplechat/controllers/GroupController.java
+++ src/main/java/dev/traydr/simplechat/controllers/GroupController.java
@@ -1,9 +1,21 @@
 package dev.traydr.simplechat.controllers;
 
+import dev.traydr.simplechat.entities.Group;
+import dev.traydr.simplechat.repositories.GroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/groups")
 public class GroupController {
+    @Autowired
+    private GroupRepository groupRepo;
+    @GetMapping("")
+    public List<Group> getGroups() {
+        return groupRepo.findAll();
+    }
 }
