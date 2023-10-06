21c8e1e1ebe40d2840ce11c8249936ee6308fef0
Traydr <Traydr@protonmail.com>
2023-10-06 16:00:54 +0300

    feat: util current date so I dont forget

---
+++ src/main/java/dev/traydr/simplechat/core/CurrentDate.java
@@ -0,0 +1,9 @@
+package dev.traydr.simplechat.core;
+
+import java.sql.Date;
+
+public class CurrentDate {
+    public static Date getCurrentDate(long offset) {
+        return new Date(System.currentTimeMillis() + offset);
+    }
+}
