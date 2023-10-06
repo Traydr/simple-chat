3f21c7c256d8facb4b71622b83a3ab1ac0f7e17f
Traydr <Traydr@protonmail.com>
2023-10-06 23:21:23 +0300

    feat: login working

--- src/main/java/dev/traydr/simplechat/controllers/AuthController.java
+++ src/main/java/dev/traydr/simplechat/controllers/AuthController.java
@@ -2,7 +2,8 @@ package dev.traydr.simplechat.controllers;
 
 import dev.traydr.simplechat.core.CurrentDate;
 import dev.traydr.simplechat.core.Password;
-import dev.traydr.simplechat.core.SessionToken;
+import dev.traydr.simplechat.core.SecureRandomEnum;
+import dev.traydr.simplechat.entities.Login;
 import dev.traydr.simplechat.entities.Token;
 import dev.traydr.simplechat.entities.User;
 import dev.traydr.simplechat.repositories.TokenRepository;
@@ -11,9 +12,12 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CurrencyEditor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -28,22 +32,32 @@ public class AuthController {
     private static final int cookieMaxAge = 7 * 24 * 60 * 60;
 
     @PostMapping("")
-    public ResponseEntity<User> Login(@Valid @RequestBody User attempt, HttpServletResponse response) {
+    public ResponseEntity<User> Login(@Valid @RequestBody Login attempt, HttpServletResponse response) {
         User user = userRepo.findByUsername(attempt.getUsername());
         if (!Password.validPassword(attempt.getPassword(), user.getPassword())) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
-        // Creating Token
-        SessionToken sessionToken = new SessionToken();
-        String stringToken = sessionToken.createToken();
+        Token existingToken = tokenRepo.findByUser(user);
+        String stringToken = "";
+        if (existingToken != null && existingToken.getExpires().after(CurrentDate.getCurrentDate(0))) {
+            existingToken.setExpires(CurrentDate.getCurrentDate(cookieMaxAge));
+            tokenRepo.save(existingToken);
+            stringToken = existingToken.getToken();
+        } else {
+            // Creating Token
+            stringToken = createToken();
 
-        // Set Token
-        Token token = new Token();
-        token.setUser(user);
-        token.setToken(stringToken);
-        token.setExpires(CurrentDate.getCurrentDate(cookieMaxAge));
-        tokenRepo.save(token);
+            // Set Token
+            Token token = new Token();
+            if (existingToken != null) {
+                token.setTid(existingToken.getTid());
+            }
+            token.setUser(user);
+            token.setToken(stringToken);
+            token.setExpires(CurrentDate.getCurrentDate(cookieMaxAge));
+            tokenRepo.save(token);
+        }
 
         // Set Cookies
         Cookie tokenCookie = new Cookie("token", stringToken);
@@ -58,5 +72,18 @@ public class AuthController {
         // Return User
         user.setPassword("");
         return ResponseEntity.ok().body(user);
+    }
+
+    public String createToken() {
+        while (true) {
+            Base64.Encoder encoder = Base64.getUrlEncoder();
+            byte[] randomBytes = new byte[30];
+            SecureRandomEnum.INSTANCE.getRandom().nextBytes(randomBytes);
+            String newToken = encoder.encodeToString(randomBytes);
+
+            if (!tokenRepo.existsByToken(newToken)) {
+                return newToken;
+            }
+        }
     }
 }
