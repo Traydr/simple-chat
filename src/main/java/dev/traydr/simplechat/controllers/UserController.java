f51b31482d3be9e4cb4dd60155047a103ec12365
Traydr <Traydr@protonmail.com>
2023-10-06 16:01:06 +0300

    feat: mostly format

--- src/main/java/dev/traydr/simplechat/controllers/UserController.java
+++ src/main/java/dev/traydr/simplechat/controllers/UserController.java
@@ -1,5 +1,6 @@
 package dev.traydr.simplechat.controllers;
 
+import dev.traydr.simplechat.core.CurrentDate;
 import dev.traydr.simplechat.core.Password;
 import dev.traydr.simplechat.entities.User;
 import dev.traydr.simplechat.exceptions.ResourceNotFoundException;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,11 +28,11 @@ public class UserController {
      */
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository userRepo;
 
     @GetMapping("")
     public List<User> getAllUsers() {
-        List<User> users = userRepository.findAll();
+        List<User> users = userRepo.findAll();
         for (User user: users) {
             user.setPassword("");
         }
@@ -39,7 +41,7 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable(value = "id") long uid) throws ResourceNotFoundException {
-        User user = userRepository.findById(uid)
+        User user = userRepo.findById(uid)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + uid));
         user.setPassword("");
         return ResponseEntity.ok().body(user);
@@ -47,7 +49,7 @@ public class UserController {
 
     @GetMapping("username/{username}")
     public ResponseEntity<User> getUserByUsername(@PathVariable(value = "username") String username) throws ResourceNotFoundException {
-        User user = userRepository.findByUsername(username);
+        User user = userRepo.findByUsername(username);
         user.setPassword("");
         return ResponseEntity.ok().body(user);
     }
@@ -55,32 +57,31 @@ public class UserController {
     @PostMapping("")
     public User createUser(@Valid @RequestBody User user) {
         user.setPassword(Password.hashPassword(user.getPassword()));
-        return userRepository.save(user);
+        user.setCreatedAt(CurrentDate.getCurrentDate(0));
+        return userRepo.save(user);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable(value = "id") Long uid,
                                            @Valid @RequestBody User userDetails) throws ResourceNotFoundException {
         // TODO: revamp this
-        User user = userRepository.findById(uid)
+        User user = userRepo.findById(uid)
                 .orElseThrow(() -> new ResourceNotFoundException("user not found for this id :: " + uid));
 
-        user.setUid(userDetails.getUid());
         user.setUsername(userDetails.getUsername());
         user.setPassword(userDetails.getPassword());
-        user.setCreatedAt(userDetails.getCreatedAt());
 
-        final User updatedUser = userRepository.save(user);
+        final User updatedUser = userRepo.save(user);
         return ResponseEntity.ok(updatedUser);
     }
 
     @DeleteMapping("/{id}")
     public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long uid, @CookieValue(value = "token") String token)
             throws ResourceNotFoundException {
-        User User = userRepository.findById(uid)
+        User User = userRepo.findById(uid)
                 .orElseThrow(() -> new ResourceNotFoundException("uid not found: " + uid));
 
-        userRepository.delete(User);
+        userRepo.delete(User);
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", Boolean.TRUE);
         return response;
