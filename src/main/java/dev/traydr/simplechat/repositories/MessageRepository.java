b6721ac36fbd312c54e1332b4cbb7582af799322
Traydr <Traydr@protonmail.com>
2023-11-05 21:39:01 +0200

    feat: add sql to find messages by group

--- src/main/java/dev/traydr/simplechat/repositories/MessageRepository.java
+++ src/main/java/dev/traydr/simplechat/repositories/MessageRepository.java
@@ -1,9 +1,13 @@
 package dev.traydr.simplechat.repositories;
 
+import dev.traydr.simplechat.entities.Group;
 import dev.traydr.simplechat.entities.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    public List<Message> findByGroup(Group group);
 }
